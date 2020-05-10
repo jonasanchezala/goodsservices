472b9c863eb39191aa71544614f52a15056786ea
Jonnathan Sanchez <jothsanchez25@gmail.com>
2020-05-09 23:50:37 -0500

    Init project

---
+++ src/main/java/com/ujaveriana/patrones/goodsservices/GoodsservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ujaveriana.patrones.goodsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujaveriana/patrones/goodsservices/GoodsservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujaveriana.patrones.goodsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
