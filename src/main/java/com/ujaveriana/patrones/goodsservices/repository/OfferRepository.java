1b9ef639207807357a83563e0b40be811c8a5d9d
developerfab <fab7696550@gmail.com>
2020-05-10 22:41:04 -0500

    add create a new offer from supplier user

---
+++ src/main/java/com/ujaveriana/patrones/goodsservices/repository/OfferRepository.java
@@ -0,0 +1,7 @@
+package com.ujaveriana.patrones.goodsservices.repository;
+
+import com.ujaveriana.patrones.goodsservices.model.Offer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, Integer> {
+}
---
+++ src/main/java/com/ujaveriana/patrones/goodsservices/service/OfferService.java
@@ -0,0 +1,36 @@
+package com.ujaveriana.patrones.goodsservices.service;
+
+import com.ujaveriana.patrones.goodsservices.model.Offer;
+import com.ujaveriana.patrones.goodsservices.model.OfferRequest;
+import com.ujaveriana.patrones.goodsservices.model.Quotation;
+import com.ujaveriana.patrones.goodsservices.model.UserApp;
+import com.ujaveriana.patrones.goodsservices.repository.OfferRepository;
+import com.ujaveriana.patrones.goodsservices.repository.QuotationRepository;
+import com.ujaveriana.patrones.goodsservices.repository.UserAppRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferService {
+    private final OfferRepository offerRepository;
+    private final QuotationRepository quotationRepository;
+    private final UserAppRepository userAppRepository;
+
+    public OfferService(OfferRepository offerRepository, QuotationRepository quotationRepository, UserAppRepository userRepository) {
+        this.offerRepository = offerRepository;
+        this.quotationRepository = quotationRepository;
+        this.userAppRepository = userRepository;
+    }
+
+    public Offer createOffer(OfferRequest offerRequest){
+        Quotation quotation = quotationRepository.findById(offerRequest.getQuotationId()).orElse(null);
+        UserApp supplier = userAppRepository.findById(offerRequest.getSupplierId()).orElse(null);
+
+        Offer offer = new Offer();
+        offer.setDecription(offerRequest.getDescription());
+        offer.setPrice(offerRequest.getPrice());
+        offer.setSupplier(supplier);
+        offer.setQuotation(quotation);
+
+        return offerRepository.save(offer);
+    }
+}
